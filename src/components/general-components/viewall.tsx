68d390aecb1ed0e3569ba1925aeedf86d9f376f1
sivamedisetti <sivamedisetti70@gmail.com>
2025-11-01 17:36:26 +0530

    Cart UI Updated

--- src/components/general-components/viewall.tsx
+++ src/components/general-components/viewall.tsx
@@ -74,7 +74,7 @@ export default function ViewAll({ webapp, setShowLogin }) {
   };
 
   useEffect(() => {
-    fetchCategories();
+    // fetchCategories();
   }, []);
 
   // Read category from localStorage
